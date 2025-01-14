4ed95189befaec18c5f0e635903b633fa34b2075
LuLuMS0206 <luciamacho00@gmail.com>
2025-01-14 18:55:24 +0100

    add tictactoe

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+const TURNS = {
+  X: 'x',
+  Y: 'y'
+}
+
+
+const board = Array(9).fill(null)
+
+function App() {
+
+  return (
+    <main className="board">
+      <h1>tic tac toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <div className="cell" key={index}>
+                <span className="cell__content"> 
+                  {index}
+                </span>
+              </div>
+            )
+          })
+        }
+      </section>
+    </main>
+    
+  )
+}
+
+export default App
