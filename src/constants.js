8a557e320dd47e1de635149977e503a3e9b8af58
LuLuMS0206 <luciamacho00@gmail.com>
2025-01-21 11:57:24 +0100

    add simbols

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,11 @@
-export const TURNS = {
+export const TURNS = { 
     X: '❌',
     O: '⚪'
 }
 
-export const WINNER_COMBOS = [
+
+
+export const winnerCombo = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
